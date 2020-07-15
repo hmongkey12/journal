2f63a7dd129dd8b28e29b8fb01f6eda3b197ca1b
hmongkey12 <khammadaolee@gmail.com>
2020-07-15 09:34:20 -0700

    updated events

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
+const text = [];
 recognition.interimResults = true;
 const leftPage = document.querySelector(".page.left-page");
 const rightPage = document.querySelector(".page.right-page");
 recognition.addEventListener("result", (event) => {
-  const message = document.createElement("p");
-  message.innerText = event.results[0][0].transcript;
-  leftPage.appendChild(message);
-  //   console.log(event.results[0][0].transcript);
+  text.push(event.results[0][0].transcript);
+  //console.log(event.results[0][0].transcript);
 });
 
-recognition.addEventListener("end", recognition.start);
+// recognition.addEventListener("end", recognition.start);
+recognition.addEventListener("end", (event) => {
+  const message = document.createElement("p");
+  message.innerText = text.join("");
+  leftPage.appendChild(message);
+  text.length = 0;
+  recognition.start();
+});
 recognition.start();
